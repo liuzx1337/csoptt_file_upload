59a7447f026ad8cee63f75af0a33a258b890aa38
刘子西 <Liuzx1337@163.com>
2018-09-06 11:25:48 +0800

    first commit.

---
+++ src/main/java/com/csoptt/FileUploadApplication.java
@@ -0,0 +1,23 @@
+package com.csoptt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author qishao
+ * date 2018-09-05
+ */
+@SpringBootApplication
+public class FileUploadApplication {
+	/**
+	 * 启动类
+	 * @param args
+	 * @return 
+	 * @author qishao
+	 * date 2018-09-05
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadApplication.class, args);
+	}
+}
