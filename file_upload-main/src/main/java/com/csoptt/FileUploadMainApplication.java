2ce5ba150beaee8420f843ffe87f1b0653e02ca6
刘子西 <Liuzx1337@163.com>
2018-11-09 14:27:14 +0800

    模块化整理

--- file_upload-main/src/main/java/com/csoptt/FileUploadMainApplication.java
+++ file_upload-main/src/main/java/com/csoptt/FileUploadMainApplication.java
@@ -2,13 +2,17 @@ package com.csoptt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 启动类
+ * 可作为微服务被Eureka发现
+ *
  * @author qishao
  * date 2018-09-05
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FileUploadMainApplication {
 
 	/**
---
+++ file_upload-restful/src/main/java/com/csoptt/controller/FileUploadController.java
@@ -0,0 +1,38 @@
+package com.csoptt.controller;
+
+import com.csoptt.utils.http.ResponseMessage;
+import com.csoptt.utils.http.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 文件上传服务restful接口
+ *
+ * @author qishao
+ * @date 2018-11-09
+ */
+@RestController
+@RequestMapping("/fileUpload")
+@Api(description = "文件上传")
+public class FileUploadController {
+    
+    /**
+     * 文件上传
+     *
+     * @param file
+     * @param fileType
+     * @return 
+     * @author qishao
+     * date 2018-11-09
+     */
+    @PostMapping("/upload")
+    @ApiOperation(value = "文件上传")
+    public ResponseMessage upload(MultipartFile file, String fileType) {
+        
+        return Result.success();
+    }
+}
